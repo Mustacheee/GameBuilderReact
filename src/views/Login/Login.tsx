e3511cddb4edd8eeeaa5b6fe3aa950f39deaeaa1
Benjamin De La Torre <bendelatorre@ymail.com>
2021-04-22 18:50:41 -0700

    Made login form with basic functionality. Need to hook up to backend

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,16 +1,67 @@
 import React from 'react';
 import styles from './Login.module.scss';
+import validationSchema from './validationSchema';
+import { Formik } from 'formik';
 import { TextField, Button } from '@material-ui/core';
 
+interface LoginForm {
+  email: string;
+  password: string;
+}
+
 const Login = () => {
+  const initialValues: LoginForm = { email: '', password: '' };
+
+  const onSubmit = (values: LoginForm) => {
+    console.log(values);
+  }
+
   return (
     <div className={styles.container}>
-      <TextField fullWidth label="Email" />
-      <TextField fullWidth label="Password" type="password" />
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        {({
+          errors,
+          handleChange,
+          handleSubmit,
+          touched,
+          values,
+        }) => {
+          return (
+            <form onSubmit={handleSubmit}>
+              <TextField
+                error={Boolean(touched.email) && Boolean(errors.email)}
+                fullWidth
+                helperText={Boolean(errors.email) && errors.email}
+                id="email"
+                label="Email"
+                name="email"
+                onChange={handleChange}
+                value={values.email}
+              />
 
-      <Button type="submit" fullWidth>
-        Login
-      </Button>
+              <TextField
+                error={Boolean(touched.password) && Boolean(errors.password)}
+                fullWidth
+                helperText={Boolean(touched.password) && errors.password}
+                id="password"
+                label="Password"
+                name="password"
+                onChange={handleChange}
+                type="password"
+                value={values.password}
+              />
+
+              <Button type="submit" fullWidth>
+                Login
+              </Button>
+            </form>
+          );
+        }}
+      </Formik>
     </div>
   );
 };
